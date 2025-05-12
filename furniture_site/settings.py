39087b959fee4746c03f6bafd32e8bde2c0fa7d5
Your Name <you@example.com>
2025-05-12 21:55:30 +0300

    Add PostgresQL

--- furniture_site/settings.py
+++ furniture_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,22 +78,21 @@ WSGI_APPLICATION = 'furniture_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'u3130097_default',
-#         'USER': 'u3130097_default',
-#         'PASSWORD': '65wwNOvOwTp3Qv2n', 
-# 	    'HOST': 'localhost',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# DATABASE_URL = postgresql://postgres:gfrEcNbpEDNHiebrZUKRVxoxvECDAonY@ballast.proxy.rlwy.net:22107/railway
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),  # Автоматически берётся из Railway
+        conn_max_age=600,
+        ssl_require=True  # Важно для Railway!
+    )
+}
 
 
 # Password validation
